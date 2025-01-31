efd476c84c48b17455704476e0f36be4d690cdd4
Goodluck-dc14 <ogbonnaiheoma48@gmail.com>
2025-01-31 22:53:00 +0200

    last commit

--- javascript_objects_scopes_closures/102-concat.js
+++ javascript_objects_scopes_closures/102-concat.js
@@ -1,12 +1,17 @@
 #!/usr/bin/node
 
-const fs = require("fs").promises;
-const { argv } = require("process");
+const fs = require('fs').promises;
+const { argv } = require('process');
 
-fs.readFile(argv[2], "utf8")
-  .then((data) => fs.writeFile(argv[4], data, "utf8"))
-  .catch((err) => console.error(err));
+async function concatFiles () {
+  try {
+    const data1 = await fs.readFile(argv[2], 'utf8');
+    const data2 = await fs.readFile(argv[3], 'utf8');
 
-fs.readFile(argv[3], "utf8")
-  .then((data) => fs.writeFile(argv[4], data, { flag: "a" }, "utf8"))
-  .catch((err) => console.error(err));
+    await fs.writeFile(argv[4], data1 + data2, 'utf8'); // Concatenate and write in one step
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+concatFiles();
